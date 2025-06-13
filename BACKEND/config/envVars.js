af9416a9e5207fd5f4ea2a52a01493549968dd0a
Pankaj Ghosh <ghoshpankaj260@gmail.com>
2025-06-13 16:27:25 +0530

    ✅ Fix movie/tv/search routes and CORS issues

--- BACKEND/config/envVars.js
+++ BACKEND/config/envVars.js
@@ -13,7 +13,7 @@ console.log("✅ Loaded MONGO_URI:", process.env.MONGO_URI);
 
 export const ENV_VARS = {
   MONGO_URI: process.env.MONGO_URI,
-  PORT: process.env.PORT || 5001,
+  PORT: process.env.PORT || 5000,
   JWT_SECRET: process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV,
   TMDB_API_KEY: process.env.TMDB_API_KEY,
